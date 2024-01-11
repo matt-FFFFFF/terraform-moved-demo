cdc25a1460010ab4e94059aa8f3e770feb21bb8d
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-11 12:43:50 +0000

    feat: move complete

--- main.tf
+++ main.tf
@@ -4,3 +4,29 @@ resource "azurerm_resource_group" "this" {
   name     = module.naming.resource_group.name_unique
   location = module.regions.regions[random_integer.region_index.result].name
 }
+
+# resource "azurerm_key_vault" "before_move" {
+#   name                = module.naming.key_vault.name_unique
+#   location            = azurerm_resource_group.this.location
+#   tenant_id           = data.azurerm_client_config.current.tenant_id
+#   sku_name            = "standard"
+#   resource_group_name = azurerm_resource_group.this.name
+# }
+
+module "avm_key_vault" {
+  source                   = "Azure/avm-res-keyvault-vault/azurerm"
+  version                  = "~> 0.5.1"
+  name                     = module.naming.key_vault.name_unique
+  location                 = azurerm_resource_group.this.location
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  resource_group_name      = azurerm_resource_group.this.name
+  enable_telemetry         = false
+  sku_name                 = "standard"
+  network_acls             = null
+  purge_protection_enabled = false
+}
+
+moved {
+  from = azurerm_key_vault.before_move
+  to   = module.avm_key_vault.azurerm_key_vault.this
+}
